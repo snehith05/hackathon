8c7c2f76c779ede0a78d96b9e90a50035667642a
Your Name <your.email@example.com>
2026-01-22 23:19:11 +0530

    Clean up schema file

--- OneDrive/Desktop/my projects/duhacks/schema.sql
+++ OneDrive/Desktop/my projects/duhacks/schema.sql
@@ -5,29 +5,22 @@
     age INTEGER,
     date_of_birth TEXT,
     occupation TEXT,
-    guardian_name TEXT,
     mobile TEXT,
     email TEXT,
     address TEXT,
 
     has_donated_before BOOLEAN,
-    donation_count INTEGER,
-    last_donation_date TEXT,
-
-    feeling_well_today BOOLEAN,
-    ate_last_4_hours BOOLEAN,
-    slept_well BOOLEAN,
-    infection_risk BOOLEAN,
-    alcohol_recent BOOLEAN
+    donation_count INTEGER
 );
-
 INSERT INTO donor_profile (
-    name, gender, age, occupation, mobile,
-    feeling_well_today, ate_last_4_hours, slept_well,
-    infection_risk, alcohol_recent
-) VALUES (
-    'MK Subrahmanya', 'Male', 19, 'Student', '8951620905',
-    1, 1, 1, 0, 0
-);
-
-SELECT * from donor_profile;
+    name,
+    gender,
+    age,
+    date_of_birth,
+    occupation,
+    mobile,
+    email,
+    address,
+    has_donated_before,
+    donation_count
+) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
